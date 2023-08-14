ef6e28409d441fd7fbf56ebfb5c88565cc55f9a8
Michael Siddiqi <bismics@gmail.com>
2023-08-14 18:47:25 +0200

    Update use CDN

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -4,7 +4,7 @@ const config: ClientConfig = {
   projectId: 'y18q52l5',
   dataset: 'production',
   apiVersion: '2023-08-13',
-  useCdn: false,
+  useCdn: true,
 };
 
 const client = createClient(config);
