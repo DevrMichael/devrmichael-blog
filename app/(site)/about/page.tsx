4b840a54ce3a83a04d607df122ced2b733863e80
Michael Siddiqi <bismics@gmail.com>
2023-08-16 15:44:30 +0200

    Add open resume in new tab

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -39,6 +39,7 @@ export default async function About() {
                     // href={`${data.resumeURL}?dl=${data.fullName}_resume`}
                     href="https://cdn.sanity.io/files/y18q52l5/production/da3aa9ab110a0e00f95ce610d194b4763b7e50c9.pdf"
                     className="flex items-center justify-center gap-x-2 bg-[#1d1d20] border border-transparent hover:border-zinc-700 rounded-md duration-200 py-2 text-center font-medium"
+                    target="_blank"
                   >
                     <BiFile className="text-base" /> Download Resumé
                   </a>
