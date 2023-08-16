e34cf0e6c92586066335a5230885d45b6b854859
Michael Siddiqi <bismics@gmail.com>
2023-08-16 15:43:03 +0200

    Add resume to the about page

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -36,7 +36,8 @@ export default async function About() {
                   />
 
                   <a
-                    href={`${data.resumeURL}?dl=${data.fullName}_resume`}
+                    // href={`${data.resumeURL}?dl=${data.fullName}_resume`}
+                    href="https://cdn.sanity.io/files/y18q52l5/production/da3aa9ab110a0e00f95ce610d194b4763b7e50c9.pdf"
                     className="flex items-center justify-center gap-x-2 bg-[#1d1d20] border border-transparent hover:border-zinc-700 rounded-md duration-200 py-2 text-center font-medium"
                   >
                     <BiFile className="text-base" /> Download Resumé
